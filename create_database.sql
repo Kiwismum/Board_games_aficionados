befb5fa2743de38d2afba5e3b19c35fe24d73783
Sharon Edwards <Kiwismum@users.noreply.github.com>
2017-06-05 14:06:05 +1200

    Added player table
    added and populated the player table ready for testing

--- create_database.sql
+++ create_database.sql
@@ -22,7 +22,46 @@ ALTER TABLE `board_games`
 ALTER TABLE `board_games`
   MODIFY `game_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
   
+--
+-- Table structure for table `players`
+--
 
+CREATE TABLE `players` (
+  `Member ID` int(20) NOT NULL,
+  `First Name` varchar(20) NOT NULL,
+  `Family Name` varchar(30) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Phone` varchar(15) NOT NULL,
+  `UserName` varchar(20) NOT NULL,
+  `Password` varchar(32) NOT NULL,
+  `admin` tinyint(4) NOT NULL
+);
+
+--
+-- Indexes for table `players`
+--
+ALTER TABLE `players`
+  ADD PRIMARY KEY (`Member ID`);
+  
+  
+--
+-- AUTO_INCREMENT for table `players`
+--
+ALTER TABLE `players`
+  MODIFY `Member ID` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- Populating players table
+--
+
+INSERT INTO `players` (`First Name`, `Family Name`, `Email`, `Phone`, `UserName`, `Password`, `admin`) VALUES
+('Fred', 'Dagg', 'fred@email.com', '021 654897', 'fred01', '362f30d4cc3897b4af7b7f938dbda151', 1),
+('George', 'Bell', 'georgieboy@gmail.com', '024 658 258', 'george', 'd4d5b3c6d954808361674100113779e9', 0),
+('Carol', 'Robins', 'carol.robins@gmail.com', '012 345 6789', 'carol', '88599f0f139eb6e7bc6b0d14b2aaa2a6', 0),
+('Nina', 'Smith', 'Nina@hotmail.com', '09 123 4567', 'nina', 'c67c2bd13856a8d8e836b79270e5d308', 0),
+('Sharon', 'Edwards', 'sharon@dreamcrafts.co.nz', '021 479 603', 'sharon', '9fb77a55258c8ac73af5a7fdfe538e5d', 0),
+('Mickey', 'Mouse', 'mickey@disney.com', '021 347639', 'mickey', 'ee979e447201c007275c2ad134c60793', 0),
+('Minnie', 'Mouse', 'minnie@disney.com', '021 646-643', 'minnie', '0c63cf595c419c4f926a467c7814aded', 0);
 
 --
 -- Table structure for table `player_games`
